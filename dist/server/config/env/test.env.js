da369feb26dbf9aecf465151f96604df23964729
mucilon <joao.g.n91@gmail.com>
2019-05-21 21:07:45 -0300

    change test host to postgres and add local-test

--- dist/server/config/env/test.env.js
+++ dist/server/config/env/test.env.js
@@ -4,9 +4,9 @@ module.exports = {
     dialect: 'postgres',
     username: 'postgres',
     password: 'pgroot',
-    host: 'localhost',
+    host: 'postgres',
     serverPort: 3000,
     pgPort: 5432,
-    dbURL: 'postgres://postgres:pgroot@localhost:5432/ts-api-test',
+    dbURL: 'postgres://postgres:pgroot@postgres:5432/ts-api-test',
     secret: 'S3cr3t'
 };
--- server/config/env/testlocal.env.ts
+++ server/config/env/testlocal.env.ts
@@ -4,9 +4,9 @@ module.exports = {
     dialect: 'postgres',
     username: 'postgres',
     password: 'pgroot',
-    host: 'postgres',
+    host: 'localhost',
     serverPort: 3000,
     pgPort: 5432,
-    dbURL: 'postgres://postgres:pgroot@postgres:5432/ts-api-test',
+    dbURL: 'postgres://postgres:pgroot@localhost:5432/ts-api-test',
     secret: 'S3cr3t'
 }
